6147c9d4de172708c0939202b9c034f48b088d04
unknown <kiasn21@gmail.com>
2020-09-29 22:49:08 +0330

    Some changes modified

--- lms/crawler.go
+++ lms/crawler.go
@@ -8,14 +8,13 @@ import (
 	"time"
 )
 
-func FetchCourses(token string){
-	var lists []string
+func FetchCourses(token string) map[string]string{
+	m := make(map[string]string)
 	url := "https://lms2.razi.ac.ir/ViewProfile.aspx"
 	req,_ := http.NewRequest("GET",url,nil)
 	cookie := http.Cookie{Name: ".ASPXAUTH", Value: token}
 	req.AddCookie(&cookie)
 	client := &http.Client{Timeout: time.Second * 10}
-	fmt.Println(req.Cookies())
 	resp,err := client.Do(req)
 	if err != nil{
 		fmt.Println(err)
@@ -23,19 +22,19 @@ func FetchCourses(token string){
 		HtmlResp := resp
 		doc,_ := goquery.NewDocumentFromReader(HtmlResp.Body)
 		doc.Find("a").Each(func(i int, s *goquery.Selection) {
-			// For each item found, get the band and title
+			// Get all courses
 			rel,_ := s.Attr("rel")
 			target,_ := s.Attr("target")
 			if rel == "noopener noreferrer" && target == "_blank" {
 				link, _ := s.Attr("href")
-				//name := s.Text()
+				name := s.Text()
 				bi := strings.Contains(link, "/Lesson/")
-				if bi {
-					lists = append(lists, "https://lms2.razi.ac.ir" + link)
-					fmt.Println("https://lms2.razi.ac.ir" + link)
+				golestan := strings.Contains(name,"گروه")
+				if bi && golestan {
+					m[name] = "https://lms2.razi.ac.ir" + link
 				}
 			}
 		})
-		fmt.Println(lists)
 	}
+	return m
 }
--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"Project/lms"
+	"fmt"
 )
 
 func main() {
-	token := ""
-	lms.FetchCourses(token)
+	asciiArt :=
+		`
+    ___       ___       ___       ___       ___       ___       ___   
+   /\  \     /\  \     /\  \     /\  \     /\__\     /\__\     /\  \  
+  /::\  \   /::\  \   _\:\  \   _\:\  \   /:/  /    /::L_L_   /::\  \ 
+ /::\:\__\ /::\:\__\ /::::\__\ /\/::\__\ /:/__/    /:/L:\__\ /\:\:\__\
+ \;:::/  / \/\::/  / \::;;/__/ \::/\/__/ \:\  \    \/_/:/  / \:\:\/__/
+  |:\/__/    /:/  /   \:\__\    \:\__\    \:\__\     /:/  /   \::/  / 
+   \|__|     \/__/     \/__/     \/__/     \/__/     \/__/     \/__/  
+
+`
+	fmt.Println(asciiArt)
+	var username,password string
+	fmt.Printf("Enter Username : ");fmt.Scanln(&username)
+	fmt.Printf("Enter Password : ");fmt.Scanln(&password)
+	token := lms.LoginLms(username,password)
+	courses := lms.FetchCourses(token)
+	for name,link := range courses {
+		fmt.Printf("Link : %v\nName : %v\n",link,name)
+	}
 }
